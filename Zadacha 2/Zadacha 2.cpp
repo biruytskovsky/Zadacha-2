e2a6517ab5bc4d06e12cde11cb5276a289aea7a3
biruytskovsky <david.biruykov.02@mail.ru>
2023-12-01 23:18:42 +0200

    Добавьте файлы проекта.

---
+++ Zadacha 2/Zadacha 2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B, C;
+    cout << "A:";
+    cin >> A;
+    cout << "B:";
+    cin >> B;
+    cout << "C:";
+    cin >> C;
+
+    
+    int volume = A * B * C;
+
+    cout << "=" << volume << endl;
+
+    return 0;
+}
